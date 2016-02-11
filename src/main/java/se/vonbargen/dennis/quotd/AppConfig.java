4261e3b7a1f66b29fd35bf06b0e422d810f36cc8
Dennis von Bargen <dennis@vonbargen.se>
2016-02-11 23:05:31 +0100

    Initial source commit

---
+++ src/main/java/se/vonbargen/dennis/quotd/AppConfig.java
@@ -0,0 +1,11 @@
+package se.vonbargen.dennis.quotd;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by dennis on 2016-02-11.
+ */
+@Configuration
+public class AppConfig {
+
+}
